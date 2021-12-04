1259da382c8045bee79954f43a5c19cc4a13a18a
Jonathan Alvares <alavres.jonthan@yahoo.com>
2021-12-04 09:56:22 +0530

    Completed the header for the website

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,9 +1,25 @@
-import "./Header.css";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 
 function Header() {
-    return (
-        <header></header>
-    );
+  return (
+    <header>
+      <Navbar variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">RemitNow</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <Nav.Link href="#home">Why RemitNow?</Nav.Link>
+              <Nav.Link href="#link">About Us</Nav.Link>
+              <Nav.Link href="#link">Try Now!!</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 }
 
 export default Header;
