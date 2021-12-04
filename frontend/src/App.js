a7c265a540c9c8aa8b41700ee2f0e0bc29b98c2c
Jonathan Alvares <alavres.jonthan@yahoo.com>
2021-12-04 16:18:30 +0530

    Added account capability to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,36 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import React, { Component } from "react";
 import Footer from "./Footer/Footer";
 import Body from "./Body/Body";
 import Header from "./Header/Header";
+import login from "./utils/login";
 
-function App() {
-  return (
-    <div className="App">
-      <Header balance="0" account="0x00000" />
-      <Body />
-      <Footer />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: "0x00000",
+    };
+  }
+
+  async UNSAFE_componentWillMount() {
+    await login();
+    if (window.web3) {
+      const account = await window.web3.eth.getAccounts();
+      this.setState({ account: account[0] });
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header balance="0" account={this.state.account} />
+        <Body />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ frontend/src/utils/login.js
@@ -0,0 +1,14 @@
+import Web3 from "web3";
+
+async function login() {
+  if (window.ethereum) {
+    window.web3 = new Web3(window.ethereum);
+    await window.ethereum.enable();
+  } else if (window.web3) {
+    window.web3 = new Web3(window.web3.currentProvider);
+  } else {
+    window.alert("No ethreum wallet detected!!!");
+  }
+}
+
+export default login;
