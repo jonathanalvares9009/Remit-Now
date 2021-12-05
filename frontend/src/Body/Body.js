af020b2b4ea00be550f9e24980681b1e417f0dcb
Jonathan Alvares <alavres.jonthan@yahoo.com>
2021-12-05 12:59:23 +0530

    Created modals

--- frontend/src/Body/Body.js
+++ frontend/src/Body/Body.js
@@ -1,7 +1,22 @@
 import "./Body.css";
+import React from "react";
+import Transfer from "./Transfer";
 
 function Body() {
-  return <main></main>;
+  const [modalShow, setModalShow] = React.useState(true);
+
+  const alert = modalShow ? (
+    ""
+  ) : (
+    <p>Transfer money by clicking the transfer button</p>
+  );
+
+  return (
+    <main>
+      {alert}
+      <Transfer show={modalShow} onHide={() => setModalShow(false)} />
+    </main>
+  );
 }
 
 export default Body;
--- frontend/src/Header/Header.js
+++ frontend/src/Header/Header.js
@@ -3,8 +3,12 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import login from "../utils/login";
 import logout from "../utils/logout";
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
 
 function Header(props) {
+  const [modalShow, setModalShow] = React.useState(false);
+
   const session = props.isLoggedIn ? (
     <Nav.Link onClick={logout}>Logout</Nav.Link>
   ) : (
@@ -12,23 +16,47 @@ function Header(props) {
   );
 
   return (
-    <header>
-      <Navbar variant="dark" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">RemitNow</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav>
-              <Nav.Link href="#">Send Money</Nav.Link>
-              <Nav.Link href="#">Balance: ${props.balance}</Nav.Link>
-              <Nav.Link href="#">Transactions</Nav.Link>
-              <Nav.Link href="#">{props.account}</Nav.Link>
-              {session}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </header>
+    <>
+      <header>
+        <Navbar variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">RemitNow</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav>
+                <Nav.Link>Send Money</Nav.Link>
+                <Nav.Link href="#">Balance: ${props.balance}</Nav.Link>
+                <Nav.Link href="#">Transactions</Nav.Link>
+                <Nav.Link onClick={() => setModalShow(true)}>
+                  {props.account}
+                </Nav.Link>
+                {session}
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+
+      <Modal
+        size="lg"
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Account Details
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Account Number: {props.account}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={() => setModalShow(false)}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
